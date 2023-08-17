c5ef95f0986cddfdf31dd3526e3f9db17da385e4
[Blaine Prickett] <[blaineprickett@gmail.com]>
2023-08-16 22:34:06 -0500

    refactored

--- app.js
+++ app.js
@@ -3,72 +3,47 @@ const config = require("./config");
 const commands = require("./lib/commands");
 const commandsList = require("./stats/commandsList.json");
 const utilities = require("./lib/utilities");
+
 const bot = new Discord.Client();
 
 bot.on("message", (msg) => {
-  if (msg.content.startsWith("!")) {
-    let messageCommand = "";
-    let messageArgsArray = [];
-    [messageCommand, ...messageArgsArray] = msg.content
-      .toString()
-      .split(" ")
-      .map((value) => value.trim())
-      .slice(0);
-    const messageArgs = messageArgsArray.join(" ");
+  if (!msg.content.startsWith("!")) return; // Return early if no command prefix
 
-    // Make sure message command is case insensitive, but rest of string should matter on case.
-    messageCommand = messageCommand.toLowerCase();
+  const [messageCommand, ...messageArgsArray] = msg.content.split(" ");
+  const messageArgs = messageArgsArray.join(" ").trim();
+  
+  const commandKey = messageCommand.toLowerCase();
 
-    // Confirm that the command called exists.
-    const validCommand = Object.prototype.hasOwnProperty.call(
-      commands,
-      messageCommand
-    );
-    // check to see if there was a minor typo in the command
-    const closeEnough = utilities.checkMinorTypo(
-      messageCommand.slice(1, messageCommand.length),
-      commandsList
-    );
-    // Create an object with data to send to the commands
-    const messageObject = {
-      user: msg.author,
-      args: messageArgs,
-      channel: msg.channel,
-    };
-    if (validCommand) {
-      commands[messageCommand](messageObject)
-        .then((response) => {
-          // Check to see if there's a channel passed as a key/value in the object.
-          if (response.message) {
-            // If so, this is the new format for the bot.
-            // If there's a channel name stored in the object, then we want to send it to
-            // that particular channel if it exists. Otherwise
-            // TODO: When we add a database to this, this needs to be able to be configured
-            // on the server level.
-            if (response.channelName) {
-              const channel = msg.member.guild.channels.find(
-                "name",
-                response.channelName
-              );
-              if (!channel) return;
-              channel.send(response.message);
-            }
+  // Confirm if the command exists
+  const validCommand = commands.hasOwnProperty(commandKey);
+  
+  // Check for minor typos in the command
+  const suggestedCommand = utilities.checkMinorTypo(messageCommand.slice(1), commandsList);
+
+  const messageObject = {
+    user: msg.author,
+    args: messageArgs,
+    channel: msg.channel,
+  };
+
+  if (validCommand) {
+    commands[commandKey](messageObject)
+      .then((response) => {
+        if (response.message) {
+          if (response.channelName) {
+            const channel = msg.guild.channels.cache.find(ch => ch.name === response.channelName);
+            if (channel) channel.send(response.message);
           } else {
-            // Otherwise, just send the response.
-            // TODO: Convert all the old responses to the new
             msg.channel.send(response);
           }
-        })
-        .catch((err) => {
-          if (err.message) {
-            msg.channel.send(err.message);
-          } else {
-            msg.channel.send(err);
-          }
-        });
-    } else if (closeEnough) {
-      msg.channel.send(`*Did you mean* \`${closeEnough}\` *?*`);
-    }
+        }
+      })
+      .catch((err) => {
+        const errorMessage = err.message || err;
+        msg.channel.send(errorMessage);
+      });
+  } else if (suggestedCommand) {
+    msg.channel.send(`*Did you mean* \`${suggestedCommand}\` *?*`);
   }
 });
 
